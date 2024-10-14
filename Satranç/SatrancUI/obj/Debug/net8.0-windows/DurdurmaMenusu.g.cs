de8b4051727082462278e809ba42c9f94a1f9c4e
mR1SY <127650188+mR1SY@users.noreply.github.com>
2024-10-14 18:32:55 +0300

    V23_14.10.24.001

--- Satranç/SatrancUI/obj/Debug/net8.0-windows/DurdurmaMenusu.g.cs
+++ Satranç/SatrancUI/obj/Debug/net8.0-windows/DurdurmaMenusu.g.cs
@@ -47,13 +47,13 @@ namespace SatrancUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SatrancUI;V1.0.0.0;component/durdurmamenusu.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SatrancUI;component/durdurmamenusu.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\DurdurmaMenusu.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace SatrancUI {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- Satranç/SatrancUI/obj/Debug/net8.0-windows/TasSecmeMenusu.g.cs
+++ Satranç/SatrancUI/obj/Debug/net8.0-windows/TasSecmeMenusu.g.cs
@@ -143,13 +143,13 @@ namespace SatrancUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SatrancUI;V1.0.0.0;component/tassecmemenusu.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SatrancUI;component/tassecmemenusu.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\TasSecmeMenusu.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -159,7 +159,7 @@ namespace SatrancUI {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
